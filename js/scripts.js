99aeab4155f921e10f0e030e8ed969a3d943b632
SkyeN <skye@dames.es>
2018-08-24 09:40:40 -0700

    add front end logic to return pricing for a 1 topping pizza

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,32 @@
 function Pizza(size, toppings) {
     this.size = size;
-    this.toppings = [];
+    this.toppings = toppings;
     this.price = 0;
 }
 
-var sizePricing = { "small": 5, "medium": 7, "large": 9 }
+var sizePricing = { "Small": 5, "Medium": 7, "Large": 9 }
 var toppingPricing = {
-    "pepperoni": 0.89,
-    "ham": 0.99,
-    "italian sauage": 0.99,
-    "mushroom": 0.49,
-    "pineapple": 0.59,
-    "spinach": 0.49,
+    "Pepperoni": 0.99,
+    "Ham": 0.99,
+    "Italian Sauage": 0.99,
+    "Mushroom": 0.49,
+    "Pineapple": 0.59,
+    "Spinach": 0.49,
 }
 
 Pizza.prototype.getPrice = function() {
-    this.price = sizePricing[this.size] + toppingPricing[this.toppings[0]];
+    this.price = sizePricing[this.size] + toppingPricing[this.toppings];
 }
 
+$(document).ready(function() {
+    $("#orderInput").submit(function(event){
+        event.preventDefault();
+        var toppings = ($("input:checkbox:checked").val());
+        var size = $("input:radio:checked").val();
+        console.log(toppings);
+        console.log(size);
+        var newPizza = new Pizza(size,toppings)
+        newPizza.getPrice();
+        console.log(newPizza.price)
+    }) 
+})
