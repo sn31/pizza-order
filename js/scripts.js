abcbce794f238bbacedee6f857806f6e052a09da
SkyeN <skye@dames.es>
2018-08-24 13:47:27 -0700

    add background image

--- js/scripts.js
+++ js/scripts.js
@@ -11,7 +11,7 @@ function Order () {
  this.tax = 0;
 }
 
-var sizePricing = { "small": 5, "medium": 7, "large": 9 }
+var sizePricing = { "Small": 5, "Medium": 7, "Large": 9 }
 var toppingPricing = {
     "pepperoni": 0.99,
     "ham": 0.99,
@@ -64,4 +64,35 @@ $(document).ready(function () {
     $("#orderButton").click(function() {
         $(".receipt").toggle("500ms");
     })
+    // Moving letters
+    $('.ml14 .letters').each(function(){
+        $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
+      });
+      
+      anime.timeline({loop: true})
+        .add({
+          targets: '.ml14 .line',
+          scaleX: [0,1],
+          opacity: [0.5,1],
+          easing: "easeInOutExpo",
+          duration: 900
+        }).add({
+          targets: '.ml14 .letter',
+          opacity: [0,1],
+          translateX: [40,0],
+          translateZ: 0,
+          scaleX: [0.3, 1],
+          easing: "easeOutExpo",
+          duration: 800,
+          offset: '-=600',
+          delay: function(el, i) {
+            return 150 + 25 * i;
+          }
+        }).add({
+          targets: '.ml14',
+          opacity: 0,
+          duration: 1000,
+          easing: "easeOutExpo",
+          delay: 1000
+        });
 })
