34a71b00f33a66376d4ce0a100f0f7fc048681c8
SkyeN <skye@dames.es>
2018-08-24 09:07:39 -0700

    create Pizza constructor and two pricing objects for size & toppings

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+function Pizza(size, toppings) {
+    this.size = size;
+    this.toppings = [];
+    this.price = 0;
+}
+
+var sizePricing = { "small": 5, "medium": 7, "large": 9 }
+var toppingPricing = {
+    "pepperoni": 0.89,
+    "ham": 0.99,
+    "italian sauage": 0.99,
+    "mushroom": 0.49,
+    "pineapple": 0.59,
+    "spinach": 0.49,
+}
